84dec4635bb1172fa42b812b98a94e6d3f122cd2
Ashish Kumar | IgnorantFox <70542481+ashishworkspace@users.noreply.github.com>
2022-12-09 20:38:20 +0530

    modified

--- client/customer-2/src/App.js
+++ client/customer-2/src/App.js
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from "react";
 const App = () => {
   const [data, setData] = useState({});
   const msg = async () => {
-    const response = await axios.get("http://<srv-name>:5000/customer2");
+    const response = await axios.get("http://server-customer-2:5000/customer2");
     console.log(response.data);
     setData(response.data);
   };
