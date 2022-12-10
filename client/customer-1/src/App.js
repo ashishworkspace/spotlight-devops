209e4a28a8d68447b46293d7e1681b2c945d56f9
Ashish Kumar | IgnorantFox <70542481+ashishworkspace@users.noreply.github.com>
2022-12-10 12:49:44 +0530

    test

--- client/customer-1/src/App.js
+++ client/customer-1/src/App.js
@@ -4,12 +4,9 @@ import React, { useState, useEffect } from "react";
 const App = () => {
   const [data, setData] = useState({});
   const msg = async () => {
-    fetch('http://server-customer-1:5000/customer1')
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        setData(data)
-      });
+    const response = await axios.get("https://jsonplaceholder.typicode.com/comments");
+    console.log(response.data);
+    setData(response.data);
   };
   useEffect(() => {
     msg();
